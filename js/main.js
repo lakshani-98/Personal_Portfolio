a08f050568a9e7f728538ac69e8ebc148bd3b7d4
Lakshani Galwatta <nilu.galwatta@gmail.com>
2024-02-04 06:35:43 +0530

    Added files

---
+++ js/main.js
@@ -0,0 +1,49 @@
+//Navigation bar effects on scroll
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//Portfolio section - Modal
+const portfolioModals = document.querySelectorAll(".porfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function (modalClick) {
+    portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener("click", () => {
+        portfolioModal(i);
+    });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+    portfolioCloseBtn.addEventListener("click", () => {
+        portfolioModals.forEach((portfolioModalView) => {
+            portfolioModalView.classList.remove("active");
+        });
+    });
+});
+
+
+//Common reveal options to create reveal animations
+ScrollReveal({      
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 100
+});
+
+//Target elements, and specify options to create reveal animations
+ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', { delay: 100, origin: 'left'});
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay: 200, origin: 'right' });
+ScrollReveal().reveal('.home .info .btn', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 100, origin: 'left', interval:50 });
+ScrollReveal().reveal('.home-img, .about-img', { delay: 100, origin: 'bottom'});
+ScrollReveal().reveal('.about .description, .copy-right', { delay: 500, origin: 'right' });
+ScrollReveal().reveal('.about .professional-list li', { delay: 600, origin: 'right', interval:100 });
+ScrollReveal().reveal('.skills-description, service-description, .contact-card, .client-swiper, .contact-left h2', { delay: 100, origin: 'left' });
+ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', { delay: 100, origin: 'bottom', interval: 100 });
+ScrollReveal().reveal('footer .group', {delay: 100, origin: 'top', interval: 50 });
