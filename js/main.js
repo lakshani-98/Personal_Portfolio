fe7fcd660593b1c227c49df4f9da27ca884e1537
Lakshani Galwatta <nilu.galwatta@gmail.com>
2024-08-06 11:48:27 +0530

    Added changes

--- js/main.js
+++ js/main.js
@@ -27,6 +27,26 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
     });
 });
 
+//Responsive navigation menu toggle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+const navItem = document.querySelector(".nav-items a")
+
+menuBtn. addEventListener("click", () => {
+navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+navigation.classList.remove("active");
+});
+
+navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
+});
+
 
 //Common reveal options to create reveal animations
 ScrollReveal({      
